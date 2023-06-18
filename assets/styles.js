b87af9e2a871a8406dba31a464a6e65c60f20707
MateusDV <mateusdevitata@gmail.com>
2023-06-17 21:13:14 -0300

    Centralizar folha de estilos

---
+++ assets/styles.js
@@ -0,0 +1,31 @@
+const styles = StyleSheet.create({
+	textbox: {
+		width: '60%',
+		height: 40,
+		padding: 8,
+		fontSize: 16,
+		borderWidth: 1,
+		borderColor: '#CCC',
+		borderRadius: 4,
+	},
+	button: {
+		backgroundColor: '#007bff',
+		paddingHorizontal: 16,
+		paddingVertical: 8,
+		borderRadius: 4,
+	},
+	buttonText: {
+		color: '#fff',
+		fontSize: 16,
+		fontWeight: '500',
+		textAlign: 'center',
+	},
+    container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+});
+
+export default styles;
