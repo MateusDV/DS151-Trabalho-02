a04003cb9a2bf414917684f70d3eecb8c67b6109
MateusDV <mateusdevitata@gmail.com>
2023-06-25 22:27:59 -0300

    Implementar tela de usuario com listagem de naves

---
+++ components/forms/usuario.jsx
@@ -0,0 +1,62 @@
+// @ts-check
+
+import React from "react";
+import { useEffect, useState } from "react";
+import { Button, ScrollView, TouchableOpacity, View } from "react-native";
+import { Text } from "react-native";
+import styles from "../../style/styles";
+
+const Usuario = ({ navigate, dbClient }) => {
+    const [navesArmazenadas, setNavesArmazenadas] = useState([]);
+    const [selecionada, setSelecionada] = useState(0);
+    
+    useEffect(() => {
+        //popula a tela ao abrir ou editar uma nave
+        async function preenche() {
+            try {
+                const res = await dbClient.obterTodasAsNaves();
+                setNavesArmazenadas(Array.from(res));
+            } catch (err) {
+                console.error(err);
+                alert("Erro ao acessar o banco de dados.");
+            }
+        }
+        preenche().catch(error => {
+            console.error(error);
+        });
+    }, []);
+
+    if (navesArmazenadas.length === 0) {
+        return <Text>Carregando naves armazenadas...</Text>
+    }
+
+    return (
+        <View style={styles.container}>
+            <Button 
+                title="Logout"
+                onPress={() => navigate('login')}
+            />
+            <Listagem naves={navesArmazenadas} selecionar={setSelecionada} />
+        </View>
+    );
+}
+
+export default Usuario;
+
+const Listagem = ({ naves, selecionar }) => {
+    return (
+        <ScrollView contentContainerStyle={styles.container}>
+            {naves.map(nave => (
+                <TouchableOpacity
+                    style={styles.listItem}
+                    key={nave.id}
+                    onPress={() => selecionar(nave.id)}
+                >
+                    <Text>
+                        {nave.model} - {nave.starship_class}
+                    </Text>
+                </TouchableOpacity>
+            ))}
+        </ScrollView>
+    );
+};
